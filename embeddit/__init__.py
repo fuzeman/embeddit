6a24a33726603d324424e4b1a1a5303ca3389ea3
Dean Gardiner <gardiner91@gmail.com>
2014-02-04 16:34:19 +1300

    Added asset bundling (with Flask-Assets)

--- embeddit/__init__.py
+++ embeddit/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.assets import Environment, Bundle
 import os
 
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -7,5 +8,59 @@ template_dir = os.path.join(base_dir, 'templates')
 static_dir = os.path.join(base_dir, 'static')
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
+# Setup Flask-Assets
+assets = Environment(app)
+
+# JS Library Bundle
+js_lib = Bundle(
+    'lib/jquery-2.1.0.min.js',
+    'lib/moment.min.js',
+    'lib/livestamp.min.js',
+
+    filters='jsmin',
+    output='gen/js_lib.js'
+)
+assets.register('js_lib', js_lib)
+
+# JS Comment + Link bundle
+js_cl = Bundle(
+    'js/comment.js',
+    'js/link.js',
+
+    filters='jsmin',
+    output='gen/js_cl.js'
+)
+assets.register('js_cl', js_cl)
+
+# JS Link Bundle
+js_l = Bundle(
+    'js/link.js',
+
+    filters='jsmin',
+    output='gen/js_l.js'
+)
+assets.register('js_l', js_l)
+
+# CSS Comment + Link bundle
+css_cl = Bundle(
+    'css/global.css',
+    'css/link.css',
+    'css/comment.css',
+
+    filters='cssmin',
+    output='gen/css_cl.css'
+)
+assets.register('css_cl', css_cl)
+
+# CSS Link Bundle
+css_l = Bundle(
+    'css/global.css',
+    'css/link.css',
+
+    filters='cssmin',
+    output='gen/css_l.css'
+)
+assets.register('css_l', css_l)
 
 import views
