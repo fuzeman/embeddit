a83206761e3570808a21c0d89ae3f4959316e46b
Dean Gardiner <gardiner91@gmail.com>
2014-02-05 01:05:53 +1300

    Removed trailing '/' from route urls

--- embeddit/views.py
+++ embeddit/views.py
@@ -12,9 +12,9 @@ def build_url(domain, fragments):
     return 'http://%s/%s' % (domain, '/'.join([x for x in fragments if x]) + '.json')
 
 
-@app.route('/r/<subreddit>/comments/<link_id>/<slug>/<comment_id>/', defaults={'domain': 'reddit.com'})
-@app.route('/<domain>/r/<subreddit>/comments/<link_id>/<slug>/<comment_id>/')
-@app.route('/<regex("\w+"):subreddit>.<domain>/comments/<link_id>/<slug>/<comment_id>/')
+@app.route('/r/<subreddit>/comments/<link_id>/<slug>/<comment_id>', defaults={'domain': 'reddit.com'})
+@app.route('/<domain>/r/<subreddit>/comments/<link_id>/<slug>/<comment_id>')
+@app.route('/<regex("\w+"):subreddit>.<domain>/comments/<link_id>/<slug>/<comment_id>')
 def view_comment(domain, subreddit, link_id, slug, comment_id):
     url = build_url(domain, [
         'r', subreddit, 'comments',
@@ -29,12 +29,12 @@ def view_comment(domain, subreddit, link_id, slug, comment_id):
     return render_template('comment.html', link=link, comment=comment)
 
 
-@app.route('/r/<subreddit>/comments/<link_id>/', defaults={'domain': 'reddit.com', 'slug': None})
-@app.route('/r/<subreddit>/comments/<link_id>/<slug>/', defaults={'domain': 'reddit.com'})
-@app.route('/<domain>/r/<subreddit>/comments/<link_id>/', defaults={'slug': None})
-@app.route('/<domain>/r/<subreddit>/comments/<link_id>/<slug>/')
-@app.route('/<regex("\w+"):subreddit>.<domain>/comments/<link_id>/', defaults={'domain': 'reddit.com', 'slug': None})
-@app.route('/<regex("\w+"):subreddit>.<domain>/comments/<link_id>/<slug>/', defaults={'domain': 'reddit.com'})
+@app.route('/r/<subreddit>/comments/<link_id>', defaults={'domain': 'reddit.com', 'slug': None})
+@app.route('/r/<subreddit>/comments/<link_id>/<slug>', defaults={'domain': 'reddit.com'})
+@app.route('/<domain>/r/<subreddit>/comments/<link_id>', defaults={'slug': None})
+@app.route('/<domain>/r/<subreddit>/comments/<link_id>/<slug>')
+@app.route('/<regex("\w+"):subreddit>.<domain>/comments/<link_id>', defaults={'domain': 'reddit.com', 'slug': None})
+@app.route('/<regex("\w+"):subreddit>.<domain>/comments/<link_id>/<slug>', defaults={'domain': 'reddit.com'})
 def view_link(domain, subreddit, link_id, slug):
     url = build_url(domain, [
         'r', subreddit, 'comments',
@@ -48,8 +48,8 @@ def view_link(domain, subreddit, link_id, slug):
     return render_template('link.html', link=link)
 
 
-@app.route('/r/<subreddit>/', defaults={'domain': 'reddit.com'})
-@app.route('/<domain>/r/<subreddit>/')
-@app.route('/<regex("\w+"):subreddit>.<domain>/', defaults={'domain': 'reddit.com'})
+@app.route('/r/<subreddit>', defaults={'domain': 'reddit.com'})
+@app.route('/<domain>/r/<subreddit>')
+@app.route('/<regex("\w+"):subreddit>.<domain>', defaults={'domain': 'reddit.com'})
 def view_subreddit(domain, subreddit):
     return '/r/%s' % subreddit
