92349c54bbba8b5760a176549d26a8ade102c2dc
Dean Gardiner <gardiner91@gmail.com>
2014-02-04 15:46:12 +1300

    Finished child comment expander

--- static/js/comment.js
+++ static/js/comment.js
@@ -1,27 +1,43 @@
 var $commentarea = $('.commentarea');
 
 $commentarea.on('click', '.noncollapsed .expand', function (){
-    hide($(this).closest('.thing'));
+    var $thing = $(this).closest('.thing'),
+        $entry = $thing.find('.entry');
+
+    $entry.find('.noncollapsed').hide();
+
+    $thing.find('.child').addClass('collapsed')
+                         .hide();
+
+    $entry.find('.collapsed').show();
 });
 
 $commentarea.on('click', '.collapsed .expand', function (){
-    show($(this).closest('.thing'));
-});
-
-function hide($thing) {
-    var $entry = $thing.find('.entry');
-
-    $entry.find('.noncollapsed').hide();
-    $thing.find('.child').hide();
-
-    $entry.find('.collapsed').show();
-}
-
-function show($thing) {
-    var $entry = $thing.find('.entry');
+    var $thing = $(this).closest('.thing'),
+        $entry = $thing.find('.entry'),
+        $child = $thing.find('.child');
 
     $entry.find('.collapsed').hide();
-
     $entry.find('.noncollapsed').show();
-    $thing.find('.child').show();
-}
+
+
+    $child.removeClass('collapsed');
+
+    // Only show if the children have been expanded
+    if(!$child.hasClass('expander-collapsed')) {
+        $child.show();
+    }
+});
+
+$('.child-expander', $commentarea).click(function() {
+    var $child = $('.child', $commentarea);
+
+    $(this).hide();
+
+    $child.removeClass('expander-collapsed');
+
+    // Only show if the comment hasn't been collapsed
+    if(!$child.hasClass('collapsed')) {
+        $child.show();
+    }
+});
